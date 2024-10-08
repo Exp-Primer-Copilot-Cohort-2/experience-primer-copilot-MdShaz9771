b108e7bf8d5d2f8bec17ff0efaa5820c3be28adc
Md Shaz <md2208033@st.jmi.ac.in>
2024-10-08 21:19:04 +0000

    Copilot commit

--- member.js
+++ member.js
@@ -1,30 +1,11 @@
 function skillsMember() {
-    const skills = [
-        'JavaScript',
-        'HTML',
-        'CSS',
-        'React',
-        'Node.js',
-        'Express',
-        'MongoDB'
-    ];
-
     return {
-        getSkills: function() {
-            return skills;
-        },
-        addSkill: function(skill) {
-            if (!skills.includes(skill)) {
-                skills.push(skill);
-            }
-        },
-        removeSkill: function(skill) {
-            const index = skills.indexOf(skill);
-            if (index > -1) {
-                skills.splice(index, 1);
-            }
+        name: 'member',
+        skills: ['html', 'css', 'javascript'],
+        showSkills: function () {
+            this.skills.forEach(skill => {
+                console.log(`${this.name} knows ${skill}`);
+            });
         }
     };
 }
-
-module.exports = skillsMember;
